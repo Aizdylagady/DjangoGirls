ba042c5c2e78d6cd49466d98677b5edf682e457f
Aizdylagady <abdylagady@gmail.com>
2021-02-24 17:13:57 +0600

    Modified views.py

--- blog/views.py
+++ blog/views.py
@@ -8,21 +8,24 @@ from .models import Post
 from .forms import PostForm
 from django.shortcuts import redirect
 
-#Post.objects.get(pk=pk)
+# Post.objects.get(pk=pk)
+
+
 from .models import Post
 from .forms import PostForm
+
+
+
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
+
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
-def post_new(request):
-    form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form})
 
 def post_new(request):
     if request.method == "POST":
@@ -51,8 +54,3 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
-
-    context = {
-        'form': form
-    }
-    return render(request, 'blog/post_edit.html', context)
